772fe279b27cbe435c1d91f62c184586ab70b7ac
cdoehn <55837480+cdoehn@users.noreply.github.com>
2020-09-01 15:27:08 +0200

    Refactor new exception tests (#77)
    Co-authored-by: Christian Henkel <post@henkelchristian.de>

--- test/unit_tests/unittest_new_exceptions.cpp
+++ test/unit_tests/unittest_new_exceptions.cpp
@@ -14,16 +14,40 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include <gtest/gtest.h>
+#include <psen_scan/build_ros_message_exception.h>
 #include <psen_scan/decrypt_password_exception.h>
+#include <psen_scan/get_ros_parameter_exception.h>
+#include <psen_scan/psen_scan_fatal_exception.h>
 
 using namespace psen_scan;
 
 namespace psen_scan_test
 {
-TEST(DecryptPasswordExceptionTest, new_build_ros_message_exception)
+TEST(BuildROSMessageExceptionTest, new_build_ros_message_exception)
+{
+  std::string except_str = "BuildROSMessageException";
+  std::unique_ptr<BuildROSMessageException> e(new BuildROSMessageException(except_str));
+  EXPECT_EQ(except_str, e->what());
+}
+
+TEST(DecryptPasswordExceptionTest, new_decrypt_password_exception)
 {
   std::string except_str = "DecryptPasswordException";
   std::unique_ptr<DecryptPasswordException> e(new DecryptPasswordException(except_str));
+  EXPECT_EQ(except_str, e->what());
+}
+
+TEST(GetROSParameterExceptionTest, new_get_ros_parameter_exception)
+{
+  std::string except_str = "GetROSParameterException";
+  std::unique_ptr<GetROSParameterException> e(new GetROSParameterException(except_str));
+  EXPECT_EQ(except_str, e->what());
+}
+
+TEST(PSENScanFatalExceptionTest, new_psen_scan_fatal_exception)
+{
+  std::string except_str = "PSENScanFatalException";
+  std::unique_ptr<PSENScanFatalException> e(new PSENScanFatalException(except_str));
   EXPECT_EQ(except_str, e->what());
 }
 }  // namespace psen_scan_test
